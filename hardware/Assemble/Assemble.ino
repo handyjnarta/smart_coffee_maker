0b51c17451c328991e4e931677d2e9f6c11ad4dd
Handy Jonarta <86114772+handyjnarta@users.noreply.github.com>
2024-07-12 09:29:35 +0700

    update

--- hardware/Assemble/Assemble.ino
+++ hardware/Assemble/Assemble.ino
@@ -8,7 +8,6 @@
 #define zerocross  5 // for boards with CHANGEBLE input pins
 
 dimmerLamp dimmer(outputPin, zerocross); // Initialize port for dimmer for ESP8266, ESP32, Arduino due boards
-//dimmerLamp dimmer(outputPin); // Initialize port for dimmer for MEGA, Leonardo, UNO, Arduino M0, Arduino Zero
 
 int thermoSO = 19;
 int thermoCS = 23;
@@ -34,11 +33,10 @@ float sum = 0.0;
 //HX711 constructor:
 HX711_ADC LoadCell(HX711_dout, HX711_sck);
 
-const int calVal_eepromAdress = 0;
 unsigned long t = 0;
 //PUMP
 int motorPin = 14; // pin to connect to motor module
-int mSpeed = 0; // variable to hold speed value
+
 int volwater = 0;
 float average = 0.0;
 
@@ -100,42 +98,31 @@ void setup() {
   volwater = readIntFromSerial();
   Serial.print(volwater);
 }
-/*
-void printSpace(int val) {
-  if ((val / 100) == 0) Serial.print(" ");
-  if ((val / 10) == 0) Serial.print(" ");
-}
-*/
+
+
 
 void updateLoadCell() {
   static boolean newDataReady = 0;
-  const int serialPrintInterval = 500; // increase value to slow down serial print activity
 
   if (LoadCell.update()) newDataReady = true;
 
   if (newDataReady) {
-    if (millis() > t + serialPrintInterval) {
       for (int k = 1; k <= 80; k++) {
         float i = LoadCell.getData();
         sum += i;
       }
       average = sum / 80;
-      if (average >= (volwater - 40)) {
-        controlMotor(0); // Stop the motor if load is too much
-      }
-      Serial.print("Load_cell output val: ");
-      Serial.println(average);
-      Serial.println("\t");
       newDataReady = 0;
       t = millis();
       sum = 0;
     }
-  }
 }
 
 void controlMotor(int speed) {
   analogWrite(motorPin, speed);
   delay(20); // Necessary delay
+  Serial.print("Motor Speed :");
+  Serial.println(speed);
 }
 
 void loop() {
@@ -145,17 +132,27 @@ void loop() {
     previousMillis = currentMillis;
 
     NilaiSuhu = thermocouple.readCelsius();
+
+      static boolean newDataReady = 0;
+
+    if (LoadCell.update()) newDataReady = true;
+
+    if (newDataReady) {
+        for (int k = 1; k <= 80; k++) {
+          float i = LoadCell.getData();
+          sum += i;
+        }
+        average = sum / 80;
+        newDataReady = 0;
+        t = millis();
+        sum = 0;
+    }
+
+
     myPID.Compute(); // Compute the PID output
 
     int preVal = dimmer.getPower();
     int newVal;
-
-   
-    controlMotor(0);
-
-    
-  // HX711_ADC Load Cell code
-  
 
 
     // Check if the temperature is within 2 degrees of the setpoint
@@ -164,20 +161,21 @@ void loop() {
       if (setpointReachedMillis == 0) {
         setpointReachedMillis = currentMillis;
       }
+      updateLoadCell();
 
 
       // Check if 10 seconds have passed since reaching the setpoint
-      if (currentMillis - setpointReachedMillis >= setpointDelay) {
+      if (currentMillis - setpointReachedMillis >= setpointDelay  && average >= (volwater)) {
         newVal = 10; // Set the dimmer power to 10 if the temperature is close to the setpoint for 10 seconds
         Output = 10;
-        mSpeed = 0;
+        controlMotor(0);
+        Serial.println("Air sudah dikeluarkan");
         dimmerSetTo10 = true; // Set the flag to true when dimmer power is set to 10%
       } else {
         newVal = (int)Output; // Otherwise, use the PID output
         dimmerSetTo10 = false; // Reset the flag if not set to 10%
-        mSpeed = 100;
-          // check for new data/start next conversion:
-        updateLoadCell();
+        controlMotor(100);
+
       }
     } else {
       // Reset the setpointReachedMillis if the temperature is out of range
@@ -185,53 +183,30 @@ void loop() {
       newVal = (int)Output; // Use the PID output
       dimmerSetTo10 = false; // Reset the flag if not set to 10%
     }
-    /*
     // Ensure the dimmer power is set smoothly
-    dimmer.setPower(newVal); // setPower(0-100%)
+    dimmer.setPower(newVal); // setPower(0-80%)
 
     if (preVal != newVal) {
-      //Serial.print("TempValue -> ");
-      printSpace(newVal);
-      //Serial.print(newVal);
-      //Serial.println("%");
+      Serial.print("TempValue -> ");
+      Serial.print(newVal);
+      Serial.println("%");
     }
-    */
+
     Serial.print("C = ");
     Serial.println(NilaiSuhu);
+    Serial.println("\t");
+
     Serial.print("Load_cell output val: ");
     Serial.println(average);
     Serial.println("\t");
-    //Serial.print("motor speed : ");
-    //Serial.println(mSpeed);
+
 
     // Handle flag reset logic
     if (dimmerSetTo10 && newVal != 10) {
       dimmerSetTo10 = false; // Reset the flag if dimmer power changes from 10
     }
   }
-
-
-  // receive command from serial terminal
-  if (Serial.available() > 0) {
-    String input = Serial.readStringUntil('\n');
-    input.trim();
-    if (input.startsWith("t")) {
-      LoadCell.tareNoDelay();
-    } else if (input.startsWith("setpoint")) {
-      Setpoint = input.substring(8).toFloat();
-      Serial.print("Setpoint updated to: ");
-      Serial.println(Setpoint);
-    } else if (input.startsWith("volwater")) {
-      volwater = input.substring(8).toInt();
-      Serial.print("volwater updated to: ");
-      Serial.println(volwater);
-    }
-  }
-
-  // check if last tare operation is complete:
-  if (LoadCell.getTareStatus() == true) {
-    Serial.println("Tare complete");
-  }
 }
+
 
 
