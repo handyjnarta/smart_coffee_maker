b2df968525067430d208f6a5ac55fdc9467fdcbb
Handy Jonarta <86114772+handyjnarta@users.noreply.github.com>
2024-07-19 10:05:26 +0700

    backend

--- software/smart_coffee_maker/lib/main.dart
+++ software/smart_coffee_maker/lib/main.dart
@@ -4,7 +4,16 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 
-void main() => runApp(MyApp());
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
